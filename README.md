6296323488d8f5bb662356b1d25fcdc57cc4f9d8
Brett Heinkel <bheinks@gmail.com>
2023-04-03 22:17:32 -0500

    Version 1.0

---
+++ README.md
@@ -0,0 +1,31 @@
+# nitbot
+```
+N - Not
+I - In
+T - The
+B - Bible
+O
+T
+```
+
+A simple Discord bot that, when mentioned either directly or in reply to a message, can tell you what words are/aren't included in the Bible.
+
+## Requirements
+```
+python = "^3.10"
+discord-py = "^2.2.2"
+python-decouple = "^3.8"
+nltk = "^3.8.1"
+contractions = "^0.1.73"
+```
+
+## Installation
+```
+pip install nitbot
+```
+
+## Usage
+```
+```
+
+## Examples
--- nitbot.py
+++ nitbot.py
@@ -3,9 +3,64 @@ from collections import defaultdict
 from pathlib import Path
 from sys import exit
 
+import contractions
+import discord
+import nltk
+from decouple import config, UndefinedValueError
+
 BIBLE_PLAINTEXT = Path('bible_en.txt')
 BIBLE_PICKLE = Path('bible_en.pickle')
+WORD_TOKENIZER = nltk.RegexpTokenizer(r'[a-zA-Z]+')
 
+try:
+    DISCORD_TOKEN = config('DISCORD_TOKEN')
+except UndefinedValueError:
+    print('ERROR: Environment file not found or DISCORD_TOKEN undefined. Please create a valid settings.ini or .env file with DISCORD_TOKEN defined.')
+    exit(-1)
+
+
+# Helper function that returns a unique, ordered list 
+def ordered_set(items):
+    return list(dict.fromkeys(items))
+
+
+class NITBot(discord.Client):
+    def __init__(self, bible_index, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bible_index = bible_index
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+    
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+        
+        if message.content.startswith('!nitb'):
+            # If used in reply to a message, fetch content of referred message
+            if message.reference is not None and not message.is_system():
+                channel = self.get_channel(message.reference.channel_id)
+                original = await channel.fetch_message(message.reference.message_id)
+                content = contractions.fix(original.content)
+                words = ordered_set(WORD_TOKENIZER.tokenize(content))
+            # Otherwise, use body of message
+            else:
+                content = contractions.fix(message.content)
+                # Exclude !nitb
+                words = ordered_set(WORD_TOKENIZER.tokenize(content)[1:])
+
+            not_in_bible = [w for w in words if not self.bible_index.find(w)]
+            if len(words) == len(not_in_bible):
+                response = 'None of these words are in the Bible.'
+            elif len(not_in_bible) == 1:
+                response = f'{not_in_bible[0]} is not in the Bible.'
+            elif len(not_in_bible) == 2:
+                response = f"{' and '.join(not_in_bible)} are not in the Bible."
+            else:
+                response = f"{', '.join(not_in_bible[:-1])} and {not_in_bible[-1]} are not in the Bible."
+
+            await message.reply(response, mention_author=True)
 
 class BibleIndex(defaultdict):
     def __init__(self, *args, **kwargs):
@@ -16,7 +71,8 @@ class BibleIndex(defaultdict):
         self[index].add(word)
     
     def find(self, word):
-        index = tuple(sorted(word))
+        print(word)
+        index = tuple(sorted(word.lower()))
         return self[index]
 
 
@@ -41,6 +97,12 @@ def main():
         # Write index to file
         with BIBLE_PICKLE.open('wb') as f:
             pickle.dump(bible_index, f, pickle.HIGHEST_PROTOCOL)
+    
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    client = NITBot(bible_index, intents=intents)
+    client.run(DISCORD_TOKEN)
         
     
 if __name__ == '__main__':
