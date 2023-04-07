87da7d67526d6edfda84cd197d36a1c6ab692b43
Brett Heinkel <bheinks@gmail.com>
2023-04-07 01:12:47 -0500

    Add license

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ O
 T
 ```
 
-A simple Discord bot that, when mentioned either directly or in reply to a message, can tell you what words are/aren't included in the Bible.
+A simple Discord bot that can tell you which words in a message aren't found in the Bible.
 
 ## Requirements
 ```
-python = "^3.10"
+python = "^3.9"
 discord-py = "^2.2.2"
 python-decouple = "^3.8"
 nltk = "^3.8.1"
@@ -20,10 +20,61 @@ contractions = "^0.1.73"
 ```
 
 ## Installation
+Clone the repository to your destination of choice:
 ```
-pip install nitbot
+git clone https://github.com/bheinks/nitbot.git
 ```
+
+Install required libraries:
+```
+pip install -r requirements.txt
+```
+
+This script uses [NLTK](https://www.nltk.org/) and requires the Punkt sentence tokenizer. Run the following in a Python shell:
+```
+import nltk
+nltk.download('punkt')
+```
+
+Use of this bot also requires a Discord bot account. Instructions can be found [here](https://discordpy.readthedocs.io/en/stable/discord.html). Once created, copy your token and keep it handy (will either be passed in via command line or set as an environment variable).
 
 ## Usage
+```
+usage: nitbot.py [-h] [-s SOURCE] [-t TOKEN]
+
+A simple Discord bot to tell you if your users' deranged ramblings are holy or not
+
+options:
+  -h, --help            show this help message and exit
+  -s SOURCE, --source SOURCE
+                        newline-delimited bible source file (defaults to KJV in English)
+  -t TOKEN, --token TOKEN
+                        Discord token (overrides DISCORD_TOKEN environment variable)
+```
+
+Example of a systemd service:
+```
+[Unit]
+Description=nitbot Discord bot service
+After=multi-user.target
+
+[Service]
+Type=simple
+Restart=always
+WorkingDirectory=/path/to/nitbot
+ExecStart=/path/to/nitbot/nitbot.py
+Environment=DISCORD_TOKEN=...
+
+[Install]
+WantedBy=multi-user.target
+```
+
+Once running, simply invite the bot to your server and reply to a message with `!nitb` to analyze the replied-to message, or supply the text to be analyzed following `!nitb` if not in reply.
 
 ## Examples
+
+Used in reply:
+![Screenshot of using nitbot in reply to a message with the text: "inspired by a dumb twitter bot, I made an equally dumb discord bot that tells you whether or not the words in a message are in the bible"](assets/screenshots/SCR-20230404-g8t.png)
+
+Used directly:
+![Screenshot of using nitbot directly with the lyrics of 100 gecs' money machine](assets/screenshots/SCR-20230404-g45.png)
--- nitbot.py
+++ nitbot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import pickle
+import argparse
 from collections import defaultdict
 from pathlib import Path
 from sys import exit
@@ -10,20 +10,10 @@ import discord
 import nltk
 from decouple import config, UndefinedValueError
 
-BIBLE_PLAINTEXT = Path('bible_en.txt')
-BIBLE_PICKLE = Path('bible_en.pickle')
+DEFAULT_BIBLE_SOURCE_PATH = Path('assets/dictionaries/bible_en.txt')
+ACTIVATION_COMMAND = '!nitb'
+# TODO: tokenize non-ASCII words for multilingual support
 WORD_TOKENIZER = nltk.RegexpTokenizer(r'[a-zA-Z]+')
-
-try:
-    DISCORD_TOKEN = config('DISCORD_TOKEN')
-except UndefinedValueError:
-    print('ERROR: Environment file not found or DISCORD_TOKEN undefined. Please create a valid settings.ini or .env file with DISCORD_TOKEN defined.')
-    exit(-1)
-
-
-# Helper function that returns a unique, ordered list 
-def ordered_set(items):
-    return list(dict.fromkeys(items))
 
 
 class NITBot(discord.Client):
@@ -32,37 +22,51 @@ class NITBot(discord.Client):
         self.bible_index = bible_index
 
     async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
+        ready_text = f'Logged in as {self.user} (ID: {self.user.id})'
+        print(ready_text)
+        print('-' * len(ready_text))
+
+        # Set bot presence
+        activity = discord.Activity(type=discord.ActivityType.watching, name='you sin in real-time')
+        await self.change_presence(activity=activity)
     
     async def on_message(self, message):
+        # Ignore messages from self
         if message.author.id == self.user.id:
             return
         
-        if message.content.startswith('!nitb'):
-            # If used in reply to a message, fetch content of referred message
+        # Act on messages starting with activation command
+        if message.content.startswith(ACTIVATION_COMMAND):
+            # If used in reply to a message, fetch content of original message
             if message.reference is not None and not message.is_system():
                 channel = self.get_channel(message.reference.channel_id)
                 original = await channel.fetch_message(message.reference.message_id)
+                
+                # Expand contractions
                 content = contractions.fix(original.content)
+
+                # Tokenize and uniquify message content
                 words = ordered_set(WORD_TOKENIZER.tokenize(content))
             # Otherwise, use body of message
             else:
+                # Expand contractions
                 content = contractions.fix(message.content)
-                # Exclude !nitb
+
+                # Tokenize and uniquify message content (excluding activation command)
                 words = ordered_set(WORD_TOKENIZER.tokenize(content)[1:])
             
-            not_in_bible = [w for w in words if not self.bible_index.find(w)]
-            if len(words) == len(not_in_bible):
+            # Filter only words that aren't found in the bible
+            words_not_in_bible = [w for w in words if w not in self.bible_index]
+            if len(words) == len(words_not_in_bible):
                 response = 'None of these words are in the Bible.'
-            elif not not_in_bible:
+            elif not words_not_in_bible:
                 response = 'All of these words are in the Bible.'
-            elif len(not_in_bible) == 1:
-                response = f'{not_in_bible[0]} is not in the Bible.'
-            elif len(not_in_bible) == 2:
-                response = f"{' and '.join(not_in_bible)} are not in the Bible."
-            else:
-                response = f"{', '.join(not_in_bible[:-1])} and {not_in_bible[-1]} are not in the Bible."
+            elif len(words_not_in_bible) == 1:
+                response = f'{words_not_in_bible[0]} is not in the Bible.'
+            elif len(words_not_in_bible) == 2:
+                response = f"{' and '.join(words_not_in_bible)} are not in the Bible."
+            else:  # > 2
+                response = f"{', '.join(words_not_in_bible[:-1])} and {words_not_in_bible[-1]} are not in the Bible."
 
             # Add cross reaction to message
             await message.add_reaction('✝️')
@@ -70,48 +74,70 @@ class NITBot(discord.Client):
             # Reply to message
             await message.reply(response, mention_author=True)
 
-class BibleIndex(defaultdict):
+
+# Store strings in a dictionary whose key is the sorted version of each string, i.e.:
+#
+# index.add('test')
+# index = {
+#     ('e', 's', 't', 't'): {'test'}
+# }
+#
+# This allows for queries in average O(1) time.
+class SortedIndex(defaultdict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     
-    def add(self, word):
-        index = tuple(sorted(word))
-        self[index].add(word)
-    
-    def find(self, word):
+    def __contains__(self, word):
         word = word.lower()
         index = tuple(sorted(word))
         return word in self[index]
 
+    def add(self, word):
+        index = tuple(sorted(word))
+        self[index].add(word)
 
-def main():
-    # Open existing bible pickle file
-    if BIBLE_PICKLE.exists():
-        with BIBLE_PICKLE.open('rb') as f:
-            bible_index = pickle.load(f)
-    # If bible pickle file does not exist, generate from plaintext
-    else:
-        # If bible plaintext does not exist, exit program
-        if not BIBLE_PLAINTEXT.exists():
-            print('ERROR: Bible plaintext/pickle files not found.')
-            return -1
-        
-        with BIBLE_PLAINTEXT.open() as f:
-            bible_index = BibleIndex(set)
-            for line in f:
-                word = line.rstrip()
-                bible_index.add(word)
-        
-        # Write index to file
-        with BIBLE_PICKLE.open('wb') as f:
-            pickle.dump(bible_index, f, pickle.HIGHEST_PROTOCOL)
+
+# Helper function that returns a unique, ordered list (as set is an unordered structure)
+def ordered_set(items):
+    return list(dict.fromkeys(items))
     
+
+def main(args):
+    # If Bible source file does not exist, exit program
+    if not args.source.exists():
+        print(f'ERROR: Bible source file {args.source.as_posix()} not found.')
+        return -1
+    
+    # Parse Bible source
+    with args.source.open() as f:
+        bible_index = SortedIndex(set)
+        for line in f:
+            # Strip newline and add to index
+            word = line.rstrip()
+            bible_index.add(word)
+    
+    # Get Discord token from args or environment
+    try:
+        discord_token = args.token or config('DISCORD_TOKEN')
+    except UndefinedValueError:
+        print('ERROR: Discord token not provided (must be passed in or defined in environment as DISCORD_TOKEN).')
+        return -1
+    
+    # Define bot intents
     intents = discord.Intents.default()
     intents.message_content = True
 
+    # Initialize and run bot
     client = NITBot(bible_index, intents=intents)
-    client.run(DISCORD_TOKEN)
+    client.run(discord_token)
         
     
 if __name__ == '__main__':
-    exit(main())
+    parser = argparse.ArgumentParser(description="A simple Discord bot to tell you if your users' deranged ramblings are holy or not")
+    parser.add_argument('-s', '--source', type=Path, default=DEFAULT_BIBLE_SOURCE_PATH,
+                        help='newline-delimited bible source file (defaults to KJV in English)')
+    parser.add_argument('-t', '--token', type=str,
+                        help='Discord token (overrides DISCORD_TOKEN environment variable)')
+    args = parser.parse_args()
+
+    exit(main(args))
